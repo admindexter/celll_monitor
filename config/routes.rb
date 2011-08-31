3c35e8c0fca59665d76c47013c9e56c4ac4e9863
Dextersmart Harry <dextersmart@gmail.com>
2011-08-31 16:57:51 +0100

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 CelllMonitor::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/faq"
+
+  get "pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
