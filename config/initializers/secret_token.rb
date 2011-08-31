814d713a396f5d7d935ec71f6dddb22378c663a9
Dextersmart Harry <dextersmart@gmail.com>
2011-08-31 16:43:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CelllMonitor::Application.config.secret_token = 'e84aaf313e4bdce2eb5fc1903930e6cbcc930c8a1f3d187ad72df805ad0edae61fd28136c4db79880b6daa7c36e0389ab139cb9a54fac80392e57114bbfd6da7'
