01273ec60936c376548c0428517bc8b2c2021433
Dextersmart Harry <dextersmart@gmail.com>
2011-09-06 15:20:01 +0100

    Added logo helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Image tag for the Celll-Monitor logo.
+  def logo
+    image_tag("logo_div_logo.jpg")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "Celll Monitor Development App"
