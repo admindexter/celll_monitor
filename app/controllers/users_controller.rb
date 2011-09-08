a67c8e620f6d6713d0efe7ad08fb09259f63c02f
Dextersmart Harry <dextersmart@gmail.com>
2011-09-08 07:18:26 +0100

    Finished first cut of the User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   layout 'general'
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @title = "Sign up"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 CelllMonitor::Application.routes.draw do
 
-  get "users/new"
+  resources :users
 
   match '/signup',  :to => 'users#new'
 
